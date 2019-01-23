a2a0f1e3d62e3e42a9851305b7abdbe8dbf1c013
vasudevareddyreddem <vasudevareddy549@gmail.com>
2019-01-23 15:58:27 +0530

    adding
    adding

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
+	'hostname' => '43.255.154.8',
+	'username' => 'psaaket',
+	'password' => 'psaaket@123',
 	'database' => 'psaaket',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
