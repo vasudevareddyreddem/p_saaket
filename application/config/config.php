7a8abfe73c42010e7c7cf025c75db77fa69e00b8
vasudevareddyreddem <vasudevareddy549@gmail.com>
2019-01-23 11:37:48 +0530

    add
    add

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/psaaket/';
+$config['base_url'] = 'http://localhost/p_saaket/';
 
 /* payment  purpose*/
 $config['keyId'] ='rzp_test_FMROR38S2ObKGw';
